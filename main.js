b306140a3375ae2a2e3d1fabfcb26a411cfb34f3
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-07-29 16:53:55 +0200

    Use v-for to display a list

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
       urlShop: 'https://www.vuevixens.org/',
       inStock: false,
       inventory: 10,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        {id: 2234, color: 'green'},
+        {id: 2235, color: 'blue'},
+      ]
+      
 
     }
   }
