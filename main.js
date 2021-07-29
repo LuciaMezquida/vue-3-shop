24d719b78e8dc2edfdb8cf8da33eb5aad007c2dc
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-07-29 16:17:36 +0200

    Add a product description in data and in the html

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const app = Vue.creatApp({
+const app = Vue.createApp({
   data(){
     return {
-      product: 'Socks'
+      product: 'Socks',
+      description: 'Your next favourite pair of socks'
     }
   }
 })
+
