56b7ab0c4c14d4bc23597d0cb32fff54c8ff60d6
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-07-29 16:50:11 +0200

    Use conditionals to display or not some info

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const app = Vue.createApp({
   data(){
     return {
       product: 'Socks',
-      description: 'Your next favourite pair of socks'
+      description: 'Your next favourite pair of socks',
+      image: './assets/images/socks_green.jpeg',
+      urlShop: 'https://www.vuevixens.org/',
+      inStock: false,
+      inventory: 10,
+
     }
   }
 })
