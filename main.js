0ca7da32f04a62061bee5f580ddcc1b9309803cc
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-08-20 12:38:15 +0200

    Add computed properties to add calculated data

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',
+      brand: 'Vue Mastery',
       description: 'Your next favourite pair of socks',
-      image: './assets/images/socks_green.jpeg',
+      selectedVariant: 0,
+      onSale: true,
       urlShop: 'https://www.vuevixens.org/',
-      inStock: true,
-      inventory: 10,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        {id: 2234, color: 'green', image: './assets/images/socks_green.jpeg'},
-        {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpeg'},
+        {id: 2234, color: 'green', image: './assets/images/socks_green.jpeg', quantity: 50},
+        {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpeg', quantity: 0},
       ]
       
 
@@ -24,9 +24,20 @@ const app = Vue.createApp({
     removeFromCart(){
       this.cart --
     },
-    updateImage(variantImage){
-      this.image = variantImage
+    updateVariant(index){
+      this.selectedVariant = index
 
+    }
+  },
+  computed: {
+    title(){
+      if(this.onSale) return this.brand + ' ' + this.product
+    },
+    image(){
+      return this.variants[this.selectedVariant].image
+    },
+    inStock(){
+      return this.variants[this.selectedVariant].quantity
     }
   }
 })
